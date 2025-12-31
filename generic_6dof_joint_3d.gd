ef66ddc14521610a3c6c92a517dbbd23335b78df
Hudson <begin123end@gmail.com>
2025-12-31 02:00:04 -0800

    v6.0 Push Physics Discovered
    Discovered Push Physics with some luck.
    
    Very crappy, must be refined, and grabbing must be implemented as well.
    
    Was getting the feeling I needed to study physics to implement it, and would you believe it was only 3 lines that needed to be in?

--- generic_6dof_joint_3d.gd
+++ generic_6dof_joint_3d.gd
@@ -1,35 +1,37 @@
 extends Node3D
-#Shoutout to dedm0zaj dev
-@onready var _hand_rigidbody_left : RigidBody3D = $LeftHandPhysics
-@onready var _hand_rigidbody_right : RigidBody3D = $RightHandPhysics
+# Shoutout to dedm0zaj dev
 
-@onready var _hand_contr_left : Node3D = $XROrigin3D/LeftController/FullBodyHandPosition_L
-@onready var _hand_contr_right : Node3D = $XROrigin3D/RightController/FullBodyHandPosition_R
+@onready var _hand_rigidbody_left: RigidBody3D = $LeftHandPhysics
+@onready var _hand_rigidbody_right: RigidBody3D = $RightHandPhysics
+@onready var _body: RigidBody3D = $CharacterBody
+
+@onready var _hand_contr_left: Node3D = $CharacterBody/XROrigin3D/LeftController/FullBodyHandPosition_L
+@onready var _hand_contr_right: Node3D = $CharacterBody/XROrigin3D/RightController/FullBodyHandPosition_R
+
 
 func _ready() -> void:
-	var xr_interface : XRInterface = XRServer.find_interface("OpenXR")
+	var xr_interface: XRInterface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
-		var vp : Viewport = get_viewport()
-		vp.use_xr = true
+		get_viewport().use_xr = true
 
 
 func _physics_process(delta: float) -> void:
-	_move_hand_rigidbody_to_contr(_hand_rigidbody_left, _hand_contr_left)
-	_move_hand_rigidbody_to_contr(_hand_rigidbody_right, _hand_contr_right)
+	var force_left: Vector3 = _move_hand_rigidbody_to_contr(_hand_rigidbody_left, _hand_contr_left)
+	var force_right: Vector3 = _move_hand_rigidbody_to_contr(_hand_rigidbody_right, _hand_contr_right)
+	_body.apply_central_force(-(force_left + force_right))
 
 
-func _move_hand_rigidbody_to_contr(hand_rigidbody: RigidBody3D, hand_contr: Node3D) -> void:
-	# 1 force hand rigidbody to hand contr
-	var move_delta : Vector3 = hand_contr.global_position - hand_rigidbody.global_position
-	
+func _move_hand_rigidbody_to_contr(hand_rigidbody: RigidBody3D, hand_contr: Node3D) -> Vector3:
+	var move_delta: Vector3 = hand_contr.global_position - hand_rigidbody.global_position
 	var coef_force := 300.0
-	hand_rigidbody.apply_central_force(move_delta * coef_force)
-	
-	# 2 torque hand rigidbody to hand contr
-	var quat_hand_rigidbody : Quaternion = hand_rigidbody.global_transform.basis.get_rotation_quaternion()
-	var quat_hand_contr : Quaternion = hand_contr.global_transform.basis.get_rotation_quaternion()
-	var quat_delta : Quaternion = quat_hand_contr * (quat_hand_rigidbody.inverse())
-	var rotation_delta : Vector3 = Vector3(quat_delta.x, quat_delta.y, quat_delta.z) * quat_delta.w
-	
+	var force: Vector3 = move_delta * coef_force
+	hand_rigidbody.apply_central_force(force)
+
+	var quat_hand_rigidbody: Quaternion = hand_rigidbody.global_transform.basis.get_rotation_quaternion()
+	var quat_hand_contr: Quaternion = hand_contr.global_transform.basis.get_rotation_quaternion()
+	var quat_delta: Quaternion = quat_hand_contr * quat_hand_rigidbody.inverse()
+	var rotation_delta: Vector3 = Vector3(quat_delta.x, quat_delta.y, quat_delta.z) * quat_delta.w
 	var coef_torque := 6.0
 	hand_rigidbody.apply_torque(rotation_delta * coef_torque)
+
+	return force
