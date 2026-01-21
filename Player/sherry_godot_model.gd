8ceeb521affacdbf5bff3fad44c305d01b2583f1
Hudson <begin123end@gmail.com>
2026-01-21 12:57:36 -0800

    v0.15 (v0.14 below) beginning implimentation from active ragdolls.
    Code was copied and pasted from elsewhere. The source had no issues with using it in your projects.
    
    Code doesn't work yet however, find out how to do that.

---
+++ Player/sherry_godot_model.gd
@@ -0,0 +1,55 @@
+extends Node3D
+
+# movement/walking/jumping stuff
+const SPEED = 50
+const DAMPING = 0.9
+
+# spring stuff
+@export var angular_spring_stiffness: float = 4000.0
+@export var angular_spring_damping: float = 80.0
+@export var max_angular_force: float = 9999.0
+
+var physics_bones = [] # all physical bones
+
+# turn it into ragdoll
+@export var ragdoll_mode := false
+
+@onready var ragdoll_skel : Skeleton3D = $Ragdoll/Armature/Skeleton3D
+@onready var pose_skel : Skeleton3D = $Pose/Armature/Skeleton3D
+@onready var physical_bone_body : PhysicalBone3D = $"Physical/Armature/Skeleton3D/Physical Bone Body"
+
+var current_delta:float
+
+func _ready():
+	pose_skel.physical_bones_start_simulation()# activate ragdoll
+	physics_bones = pose_skel.get_children().filter(func(x): return x is PhysicalBone3D) # get all the physical bones
+
+func _physics_process(delta):
+	current_delta = delta
+	if not ragdoll_mode:# if not in ragdoll mode
+		# walking control
+		var dir = Vector3.ZERO
+		if Input.is_action_pressed("move_forward"):
+			dir += ragdoll_skel.global_transform.basis.z
+		if Input.is_action_pressed("move_left"):
+			dir += ragdoll_skel.global_transform.basis.x
+		if Input.is_action_pressed("move_right"):
+			dir -= ragdoll_skel.global_transform.basis.x
+		if Input.is_action_pressed("move_backward"):
+			dir -= ragdoll_skel.global_transform.basis.z
+		dir = dir.normalized()
+
+# spring related function
+func hookes_law(displacement: Vector3, current_velocity: Vector3, stiffness: float, damping: float) -> Vector3:
+	return (stiffness * displacement) - (damping * current_velocity)
+# Confirmed Required Above
+func _on_skeleton_3d_skeleton_updated() -> void:
+	if not ragdoll_mode:# if not in ragdoll mode
+		# rotate the physical bones toward the animated bones rotations using hookes law
+		for b:PhysicalBone3D in physics_bones:
+			var target_transform: Transform3D = ragdoll_skel.global_transform * ragdoll_skel.get_bone_global_pose(b.get_bone_id())
+			var current_transform: Transform3D = pose_skel.global_transform * pose_skel.get_bone_global_pose(b.get_bone_id())
+			var rotation_difference: Basis = (target_transform.basis * current_transform.basis.inverse())
+			var torque = hookes_law(rotation_difference.get_euler(), b.angular_velocity, angular_spring_stiffness, angular_spring_damping)
+			torque = torque.limit_length(max_angular_force)
+			b.angular_velocity += torque * current_delta
