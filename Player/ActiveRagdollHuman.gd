feaaacb479108ed3c59d4d5b1826d75c5e6af4fd
Hudson <begin123end@gmail.com>
2026-01-22 19:46:55 -0800

    v0.16.1 Switched to Jolt
    Switched to Jolt Physics. The Ragdoll is able to hold it's position this way, however, my usual method of freezing the Hips in place doesn't work, so another method for that will need to be discovered.

--- Player/ActiveRagdollHuman.gd
+++ Player/ActiveRagdollHuman.gd
@@ -1,22 +1,23 @@
 extends Node3D
 
+@export var animated_skel : Skeleton3D
+@export var physical_skel : Skeleton3D
+
 @export var linear_spring_stiffness: float = 100
 @export var linear_spring_damping: float = 10
 @export var angular_spring_stiffness: float = 50.0
 @export var angular_spring_damping: float = 20.0
-@export var max_angular_force: float = 9999.0
 
-var physics_bones = []
-
-@onready var physical_skel : Skeleton3D = $Ragdoll/Armature/Skeleton3D
-@onready var animated_skel : Skeleton3D = $Pose/Armature/Skeleton3D
-@onready var physical_bone_body : PhysicalBone3D = $"Ragdoll/Armature/Skeleton3D/Physical Bone Hips"
+var physics_bones
 
 var current_delta:float
 
 func _ready():
 	physical_skel.physical_bones_start_simulation()
-	physics_bones = physical_skel.get_children().filter(func(x): return x is PhysicalBone3D) # get all the physical bones
+	physics_bones = physical_skel.get_children().filter(func(x): return x is PhysicalBone3D)
+
+func _process(delta):
+	pass
 
 func _physics_process(delta):
 	for b:PhysicalBone3D in physics_bones:
@@ -29,7 +30,6 @@ func _physics_process(delta):
 		
 		var rotation_difference: Basis = (target_transform.basis * current_transform.basis.inverse())
 		var torque = hookes_law(rotation_difference.get_euler(), b.angular_velocity, angular_spring_stiffness, angular_spring_damping)
-		torque = torque.limit_length(max_angular_force)
 		b.angular_velocity += torque * current_delta
 
 func hookes_law(displacement: Vector3, current_velocity: Vector3, stiffness: float, damping: float) -> Vector3:
