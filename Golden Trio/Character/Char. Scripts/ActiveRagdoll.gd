5bde52e25d089712a84ebe80d9f9679ad5290b79
Hudson <myendemail@proton.me>
2026-04-29 22:37:12 -0700

    v0.35

--- Golden Trio/Character/Char. Scripts/ActiveRagdoll.gd
+++ Golden Trio/Character/Char. Scripts/ActiveRagdoll.gd
@@ -1,31 +1,35 @@
 extends Node3D
+var walking = false # if it is walking
+@export var angular_spring_stiffness: float = 4000.0
+@export var angular_spring_damping: float = 80.0
+@export var max_angular_force: float = 9999.0
+var physics_bones = [] # all physical bones
+# turn it into ragdoll
+@export var ragdoll_mode := false
 @export var pose_skel : Skeleton3D
 @export var physical_skel : Skeleton3D
-@export var linear_spring_stiffness: float = 1000
-@export var linear_spring_damping: float = 2.0
-@export var angular_spring_stiffness: float = 50.0
-@export var angular_spring_damping: float = 2.0
-@export var max_linear_force: float = 2000
-var physics_bones
+@onready var animated_skel : Skeleton3D = $Animated/Armature/Skeleton3D
+@onready var camera_pivot = $CameraPivot
+@onready var animation_tree = $Animated/AnimationTree
+@onready var physical_bone_body : PhysicalBone3D = $"Physical/Armature/Skeleton3D/Physical Bone Body"
 var current_delta:float
 func _ready():
-	physical_skel.physical_bones_start_simulation()
-	physics_bones = physical_skel.get_children().filter(func(x): return x is PhysicalBone3D)
-func _process(delta):
-	pass
+	physical_skel.physical_bones_start_simulation()# activate ragdoll
+	physics_bones = physical_skel.get_children().filter(func(x): return x is PhysicalBone3D) # get all the physical bones
 func _physics_process(delta):
-	for b in physics_bones:
+	current_delta = delta
+	#play walking animation/idle
+	if walking:animation_tree.set("parameters/walking/blend_amount",1)
+	else:animation_tree.set("parameters/walking/blend_amount",0)
+	#rotate the character toward the camera direction
+	animated_skel.rotation.y = camera_pivot.rotation.y
+func hookes_law(displacement: Vector3, current_velocity: Vector3, stiffness: float, damping: float) -> Vector3:
+	return (stiffness * displacement) - (damping * current_velocity)
+func _on_skeleton_3d_skeleton_updated() -> void:
+	for b:PhysicalBone3D in physics_bones:
 		var target_transform: Transform3D = pose_skel.global_transform * pose_skel.get_bone_global_pose(b.get_bone_id())
 		var current_transform: Transform3D = physical_skel.global_transform * physical_skel.get_bone_global_pose(b.get_bone_id())
 		var rotation_difference: Basis = (target_transform.basis * current_transform.basis.inverse())
-		var position_difference: Vector3 = target_transform.origin - current_transform.origin
-		if position_difference.length_squared() > 1.0:
-			b.global_position = target_transform.origin
-		else:
-			var force: Vector3 = hookes_law(position_difference, b.linear_velocity, linear_spring_stiffness, linear_spring_damping)
-			force = force.limit_length(max_linear_force)
-			b.linear_velocity += (force * delta)
 		var torque = hookes_law(rotation_difference.get_euler(), b.angular_velocity, angular_spring_stiffness, angular_spring_damping)
+		torque = torque.limit_length(max_angular_force)
 		b.angular_velocity += torque * current_delta
-func hookes_law(displacement: Vector3, current_velocity: Vector3, stiffness: float, damping: float) -> Vector3:
-	return (stiffness * displacement) - (damping * current_velocity)
